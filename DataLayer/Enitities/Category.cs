9b39e7867455b46cd32f6227162ccc5931fc43d6
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-06-11 16:47:30 +0430

    firstpage ui completed

--- DataLayer/Enitities/Category.cs
+++ DataLayer/Enitities/Category.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer
+namespace DataLayer.Enitities
 {
-    public class Class1
+    public class Category
     {
+
     }
 }
