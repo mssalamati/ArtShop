11c9d737d53683fc4e7b79fcfbba72f4a6d385eb
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-06-24 21:02:11 +0430

    to adib

--- ArtShop/App_Start/BundleConfig.cs
+++ ArtShop/App_Start/BundleConfig.cs
@@ -24,7 +24,6 @@ namespace ArtShop
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
