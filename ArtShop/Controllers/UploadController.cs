da97943e93613ad749a02920986ae49ad7964ac9
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-07-01 02:49:09 +0430

    adib updates

---
+++ ArtShop/Controllers/UploadController.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtShop.Controllers
+{
+    public class UploadController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Setep1()
+        {
+            return PartialView();
+        }
+        public ActionResult Setep2()
+        {
+            return PartialView();
+        }
+        public ActionResult Setep3()
+        {
+            return PartialView();
+        }
+        public ActionResult Setep4()
+        {
+            return PartialView();
+        }
+        public ActionResult Setep5()
+        {
+            return PartialView();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
