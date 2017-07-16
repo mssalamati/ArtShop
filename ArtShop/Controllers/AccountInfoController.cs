2be54f6f86b576bebcb5e6cbb1d70eb27c06476b
root <adb.dehghan@gmail.com>
2017-07-16 07:04:33 +0430

    profile info

--- ArtShop/Controllers/AccountInfoController.cs
+++ ArtShop/Controllers/AccountInfoController.cs
@@ -55,5 +55,22 @@ namespace ArtShop.Controllers
 
             return View(model);
         }
+
+        public ActionResult ProfileInformation()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var userProfile = db.UserProfiles.Find(userId);
+
+            AccountInfoViewModel model = new AccountInfoViewModel();
+            model.FirstName = userProfile.FirstName;
+            model.LastName = userProfile.LastName;
+            model.Email = userProfile.ApplicationUserDetail.Email;
+            model.profileType = userProfile.profileType;
+            model.ReceiveNewArtEmail = userProfile.ReceiveNewArtEmail;
+            model.MailingList = userProfile.MailingList;
+
+            return View(model);
+        }
     }
 }
---
+++ ArtShop/Models/ProfileInformationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using DataLayer.Enitities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArtShop.Models
+{
+    public class ProfileInformationViewModel
+    {
+        [Display(Name = "Facebook")]
+        public string Facebook { get; set; }
+        [Display(Name = "Twitter")]
+        public string Twitter { get; set; }
+        [Display(Name = "Pinterest")]
+        public string Pinterest { get; set; }
+        [Display(Name = "Tumblr")]
+        public string Tumblr { get; set; }
+        [Display(Name = "Instagram")]
+        public string Instagram { get; set; }
+        [Display(Name = "GooglePlus")]
+        public string GooglePlus { get; set; }
+        [Display(Name = "My Website")]
+        public string Website { get; set; }
+        [Display(Name = "AboutMe")]
+        public string AboutMe { get; set; }
+        [Display(Name = "Education")]
+        public string Education { get; set; }
+        [Display(Name = "Events")]
+        public string Events { get; set; }
+        [Display(Name = "Exhibitions")]
+        public string Exhibitions { get; set; }
+        [Display(Name = "Country")]
+        public virtual Country country { get; set; }
+        [Display(Name = "City")]
+        public virtual string City { get; set; }
+        [Display(Name = "Region")]
+        public virtual string Region { get; set; }
+        [Display(Name = "ZipCode")]
+        public virtual string ZipCode { get; set; }
+    }
+}
