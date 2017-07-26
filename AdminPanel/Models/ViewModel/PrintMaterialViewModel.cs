e548d204bb34024ffb01b33561d11ac5c4c5cdc4
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-07-26 12:30:53 +0430

    printoption edit

---
+++ AdminPanel/Models/ViewModel/PrintMaterialViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminPanel.Models.ViewModel
+{
+    public class PrintMaterialViewModel
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public int parentid { get; set; }
+        public List<PrintMaterialTranslationViewModel> Translations { get; set; }
+    }
+
+    public class PrintMaterialTranslationViewModel
+    {
+        public string languageId { get; set; }
+        public string title { get; set; }
+    }
+} 
