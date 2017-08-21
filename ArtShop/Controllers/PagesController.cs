11b82c3af53763d96c3693650937fca203e3456e
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-08-21 05:21:58 +0430

    pages

--- ArtShop/Controllers/PagesController.cs
+++ ArtShop/Controllers/PagesController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DataLayer.Extentions;
 namespace ArtShop.Controllers
 {
-    public class PagesController : Controller
+    public class PagesController : BaseController
     {
         public ActionResult About()
         {
-            return View();
+            var page = db.SitePages.Include("Translations").SingleOrDefault(x => x.DefaultTitle == "About");
+            return View(page.Current());
         }
     }
 }
