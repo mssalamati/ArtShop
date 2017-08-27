b7b7c27bfddbb9ecf88d9154e46b8f4162f3dc09
root <adb.dehghan@gmail.com>
2017-08-27 06:01:18 +0430

    blog

--- Blog/Models/HeaderPhoto.cs
+++ Blog/Models/HeaderPhoto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace Blog.Objects
 {
     public class HeaderPhoto
     {
+        [Key]
         public int Id { get; set; }
         public string Path { get; set; }
         public string Description { get; set; }
