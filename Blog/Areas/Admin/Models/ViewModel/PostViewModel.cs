10c20c1fb51118e09563d75102b85daaa435d94d
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-08-27 06:05:54 +0430

    po

---
+++ Blog/Areas/Admin/Models/ViewModel/PostViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Areas.Admin.Models.ViewModel
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        [AllowHtml]
+        public string Content { get; set; }
+        public int Category { get; set; }
+        public string Tags { get; set; }
+        public string[] Artworks { get; set; }
+        public string Title { get; set; }
+    }
+}
