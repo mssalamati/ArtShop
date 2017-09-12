942fb010fde80f246cff0ed471121b68e8ac4a9d
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-09-13 01:48:17 +0430

    dbchange

--- DataLayer/Enitities/UserProfile.cs
+++ DataLayer/Enitities/UserProfile.cs
@@ -25,6 +25,7 @@ namespace DataLayer.Enitities
         public virtual bool isIDConfirmed { get; set; }
         public virtual string IdConfirmedBy { get; set; }
         public virtual string Region { get; set; }
+        public virtual decimal Account { get; set; }
         public virtual string ZipCode { get; set; }
         public bool MailingList { get; set; }
         public bool ReceiveNewArtEmail { get; set; }
