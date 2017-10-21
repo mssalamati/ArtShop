9d7fcfc711703a94771abb719536bcc15dc1c0df
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-10-21 18:46:21 +0330

    b

--- ArtShop/Controllers/CardController.cs
+++ ArtShop/Controllers/CardController.cs
@@ -38,7 +38,7 @@ namespace ArtShop.Controllers
             {
                 ViewBag.Street = user.billingInfo.Street;
                 ViewBag.City = user.billingInfo.City;
-                ViewBag.Country = user.billingInfo.country.Id;
+                ViewBag.Country = user.billingInfo.CountryId;
                 ViewBag.Region = user.billingInfo.Region;
                 ViewBag.ZipCode = user.billingInfo.ZipCode;
                 ViewBag.PhoneNumber = user.billingInfo.PhoneNumber;
