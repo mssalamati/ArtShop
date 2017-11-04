43612f2bbd0a6f39ab5fe7a6573eab1451a7dc99
ms.salamati@gmail.com <ms.salamati@gmail.com>
2017-11-04 17:30:07 +0330

    add to card solved, no entity mode added

--- ArtShop/Controllers/CardController.cs
+++ ArtShop/Controllers/CardController.cs
@@ -281,8 +281,9 @@ namespace ArtShop.Controllers
             return View(order);
         }
 
-
-
+      
+        [OutputCache(VaryByParam = "*", Duration = 0, NoStore = true)]
+        [NoCache]
         public ActionResult AddToCart(int id, Ordertype type)
         {
             var addedAlbum = db.Products.Find(id);
@@ -403,5 +404,20 @@ namespace ArtShop.Controllers
             return apiContext;
         }
 
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class NoCacheAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            filterContext.HttpContext.Response.Cache.SetValidUntilExpires(false);
+            filterContext.HttpContext.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            filterContext.HttpContext.Response.Cache.SetNoStore();
+
+            base.OnResultExecuting(filterContext);
+        }
     }
 }
